558806bde24d3720976dc09de83cc36ed281a9f1
Cameron Griffin (He/Him) <101752687+CodrCam@users.noreply.github.com>
2022-11-15 09:37:03 -0800

    Create RandomClass.py

---
+++ RandomClass.py
@@ -0,0 +1,10 @@
+# Python3 program to demonstrate
+# the use of sample() function on the members of class.
+  
+# import random 
+from random import sample
+  
+# Prints randomly ordered list of names. 
+list1 = ["Gregory", "Cameron", "Jenae", "Scott", "Alex", "Eric", "Kevin", "Grant", "Jose", "Prince Henry"] 
+  
+print(sample(list1,10))
